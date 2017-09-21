035323ab8fcc2d7426b013464be5b173156e6fd1
Saamer Mansoor <i@saamer.me>
2017-09-21 14:52:16 -0600

    Both on and off work

--- ParticlePhotonLED/ParticlePhotonLED/ParticlePhotonLED.cs
+++ ParticlePhotonLED/ParticlePhotonLED/ParticlePhotonLED.cs
@@ -12,9 +12,10 @@ namespace ParticlePhotonLED
     {
         public App()
         {
-			var ChangeLedCommand = new Command<string>(ChangeLed);
-			var onButton = new Button{ Text = "ON", Command=ChangeLedCommand };
-			var offButton = new Button { Text = "OFF" };
+			var ChangeLedCommandOn = new Command<string>(ledOn);
+			var ChangeLedCommandOff = new Command<string>(ledOff);
+            var onButton = new Button{ Text = "ON", Command=ChangeLedCommandOn };
+            var offButton = new Button { Text = "OFF", Command=ChangeLedCommandOff };
 			//onButton.Clicked += ChangeLed("on");
             //offButton.Clicked += ChangeLed(this, onButton.Text);
    //         onButton.Clicked += async (sender, e) => {
@@ -65,8 +66,9 @@ namespace ParticlePhotonLED
             MainPage = new NavigationPage(content);
         }
 
-        public void ChangeLed(string changeValue="on")
+        public void ledOn(string changeValue="on")
         {
+            changeValue = "on";
             string accessToken = "e54ec36b6b139319129d8cd075cb88f095a9dce7"; //This is your Particle Cloud Access Token
             string deviceId = "28003d001847343338333633"; //This is your Particle Device Id
             string partilceFunc = "led"; //This is the name of your Particle Function
@@ -85,6 +87,27 @@ namespace ParticlePhotonLED
 
             var result = client.PostAsync("v1/devices/" + deviceId + "/" + partilceFunc, content);
         }
+		public void ledOff(string changeValue = "off")
+		{
+			changeValue = "off";
+			string accessToken = "e54ec36b6b139319129d8cd075cb88f095a9dce7"; //This is your Particle Cloud Access Token
+			string deviceId = "28003d001847343338333633"; //This is your Particle Device Id
+			string partilceFunc = "led"; //This is the name of your Particle Function
+
+			HttpClient client = new HttpClient
+			{
+				BaseAddress =
+				new Uri("https://api.particle.io/")
+			};
+
+			var content = new FormUrlEncodedContent(new[]
+			{
+				new KeyValuePair<string, string>("access_token", accessToken),
+				new KeyValuePair<string, string>("args", changeValue )
+			});
+
+			var result = client.PostAsync("v1/devices/" + deviceId + "/" + partilceFunc, content);
+		}
 
         protected override void OnStart()
         {
