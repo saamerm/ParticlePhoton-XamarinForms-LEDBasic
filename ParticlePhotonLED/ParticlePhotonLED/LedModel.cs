42620bce9fee4a5c90ad39d9c0783af1f300ad17
Saamer Mansoor <i@saamer.me>
2017-09-21 13:07:16 -0600

    Works on iOS

---
+++ ParticlePhotonLED/ParticlePhotonLED/LedModel.cs
@@ -0,0 +1,8 @@
+﻿using System;
+namespace ParticlePhotonLED
+{
+    public class LedModel
+    {
+        public string args { get; set; }
+    }
+}
---
+++ ParticlePhotonLED/ParticlePhotonLED/ParticlePhotonLED.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using Xamarin.Forms;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace ParticlePhotonLED
+{
+    public class App : Application
+    {
+        public App()
+        {
+			var ChangeLedCommand = new Command<string>(ChangeLed);
+			var onButton = new Button{ Text = "ON", Command=ChangeLedCommand };
+			var offButton = new Button { Text = "OFF" };
+			//onButton.Clicked += ChangeLed("on");
+            //offButton.Clicked += ChangeLed(this, onButton.Text);
+   //         onButton.Clicked += async (sender, e) => {
+			//	HttpClient client = new HttpClient();
+   //             //client.DefaultRequestHeaders.Add("args", "on");
+   //             var jObject = "{\"args\":\"on\"}";
+			//	var uri = "https://api.particle.io/v1/devices/28003d001847343338333633/led?access_token=e54ec36b6b139319129d8cd075cb88f095a9dce7";
+   //             var model = new LedModel(){args="on"};
+   //             var stringContent = new StringContent(JsonConvert.SerializeObject(model), Encoding.UTF8, "application/json");
+			//	//var stringContent = new StringContent(jObject.ToString());
+			//	try
+   //             {
+			//		var response = await client.PostAsync(uri, stringContent);
+			//		//string obstring = await client.PostAsync(uri,);
+			//	}
+   //             catch (Exception ex)
+   //             {
+   //                 var z = 0;
+   //             }
+			//	//string obstring = await client.GetStringAsync(uri);
+			//	//RootObject weatherlist = JsonConvert.DeserializeObject<RootObject>(obstring);
+			//	////weatherText.Text = "Here's the city" + weatherlist.name;
+			//	////Complicating a little more
+			//	//var weather1 = new Weather();
+			//	//weatherlist.weather.Add(weather1);
+			//	//weatherText.Text = "Looks like there is a " + weatherlist.weather[0].description + " in " + weatherlist.name;
+			//	var x = 5;
+			//	var y = 5;
+			//};
+			// The root page of your application
+			var content = new ContentPage
+            {
+                Title = "ParticlePhotonLED",
+                Content = new StackLayout
+                {
+                    VerticalOptions = LayoutOptions.Center,
+                    Children = {
+						new Label {
+							HorizontalTextAlignment = TextAlignment.Center,
+							Text = "Welcome to being able to control Particle Photon using Xamarin Forms!"
+						}, 
+                        onButton, 
+                        offButton
+                    }
+                }
+            };
+
+            MainPage = new NavigationPage(content);
+        }
+
+        public void ChangeLed(string changeValue="on")
+        {
+            string accessToken = "e54ec36b6b139319129d8cd075cb88f095a9dce7"; //This is your Particle Cloud Access Token
+            string deviceId = "28003d001847343338333633"; //This is your Particle Device Id
+            string partilceFunc = "led"; //This is the name of your Particle Function
+
+            HttpClient client = new HttpClient
+            {
+                BaseAddress =
+                new Uri("https://api.particle.io/")
+            };
+
+            var content = new FormUrlEncodedContent(new[]
+            {
+                new KeyValuePair<string, string>("access_token", accessToken),
+                new KeyValuePair<string, string>("args", changeValue )
+            });
+
+            var result = client.PostAsync("v1/devices/" + deviceId + "/" + partilceFunc, content);
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
